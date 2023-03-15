f5776c63323ef319d7dbd3faa8ffe070a2d94aec
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-03-15 16:44:07 -0300

    implemented a method to get a list by id

--- MoviesAPI/Controllers/MovieController.cs
+++ MoviesAPI/Controllers/MovieController.cs
@@ -9,11 +9,22 @@ namespace MoviesAPI.Controllers
     {
 
         private static List<Movie> movies = new List<Movie>();
+        public static int id = 1;
 
         [HttpPost]
         public void AddMovie([FromBody] Movie movie)
         {
+            movie.Id = id++;
             movies.Add(movie);
         }
+
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return movies;
+        }
+
+        [HttpGet("{id}")]
+        public Movie GetMovieById(int id) => movies.FirstOrDefault(movie => movie.Id == id);
     }
 }
--- MoviesAPI/Models/Movie.cs
+++ MoviesAPI/Models/Movie.cs
@@ -5,12 +5,13 @@ namespace MoviesAPI.Models
     public class Movie
     {
         [Required(ErrorMessage = "O campo titulo e obrigatorio!")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
         [Required(ErrorMessage = "O campo diretor e obrigatorio!")]
-        public string Director { get; set; }
+        public string? Director { get; set; }
         [Required(ErrorMessage = "O campo genero e obrigatorio!")]
-        public string Genre { get; set; }
+        public string? Genre { get; set; }
         [Range(1, 600, ErrorMessage = "O campo duracao precisar ter de 0 a 600 minutos!")]
         public int Duration { get; set; }
+        public int Id { get; internal set; }
     }
 }
